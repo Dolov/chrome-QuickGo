7f84358fcc5fdac7138ab8e464152a6b3c8c1ad2
石松岩.前端架构组 <songyan_shi@163.com>
2023-07-18 22:09:32 +0800

    openOptionsPage

--- background.ts
+++ background.ts
@@ -1,2 +1,7 @@
 
-console.log('chrome: ', chrome);
+
+chrome.action.onClicked.addListener(activeTab => {
+  // const newURL = "https://baidu.com";
+  // chrome.tabs.create({ url: newURL });
+  chrome.runtime.openOptionsPage();
+});
