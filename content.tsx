b68e4b2b0dfe5ef04af56f2168dfa21f3209103a
石松岩.前端架构组 <songyan_shi@163.com>
2023-10-08 15:12:04 +0800

    a 标签添加 _blank，在新标签页打开

--- content.tsx
+++ content.tsx
@@ -9,6 +9,19 @@ const content: React.FC<contentProps> = props => {
 
   useOpenPage()
 
+  React.useEffect(() => {
+    document.addEventListener('mouseover', event => {
+      // @ts-ignore
+      const tagName = (event.target.tagName as string).toLowerCase()
+      if (tagName !== 'a') return
+      // @ts-ignore
+      const target = event.target.getAttribute("target")
+      if (target === "_blank") return
+      // @ts-ignore
+      event.target.setAttribute("target", "_blank")
+    })
+  }, [])
+
   return null
 }
 
